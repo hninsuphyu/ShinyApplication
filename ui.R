aff7a21c783625cc4461d2d3b096be88e00fa720
HNIN SU <hninsuphyu@ucsm.edu.mm>
2020-09-20 12:40:54 -1200

    ShinyApplication

---
+++ ui.R
@@ -0,0 +1,46 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for miles per gallon app ----
+ui <- fluidPage(
+    
+    # App title ----
+    titlePanel("Miles Per Gallon"),
+    
+    # Sidebar layout with input and output definitions ----
+    sidebarLayout(
+        
+        # Sidebar panel for inputs ----
+        sidebarPanel(
+            
+            # Input: Selector for variable to plot against mpg ----
+            selectInput("variable", "Variable:",
+                        c("Cylinders" = "cyl",
+                          "Transmission" = "am",
+                          "Gears" = "gear")),
+            
+            # Input: Checkbox for whether outliers should be included ----
+            checkboxInput("outliers", "Show outliers", TRUE)
+            
+        ),
+        
+        # Main panel for displaying outputs ----
+        mainPanel(
+            
+            # Output: Formatted text for caption ----
+            h3(textOutput("caption")),
+            
+            # Output: Plot of the requested variable against mpg ----
+            plotOutput("mpgPlot")
+            
+        )
+    )
+)
